277d5caf0dcd5c2a589b7f0d69a246203639b9d5
Bazalter <egor-zaikin3@yandex.ru>
2024-01-14 17:12:30 +0300

    Home work 1

--- Занятие 1. Основы ООП/3. Домашние задания/task8_Glass/task.py
+++ Занятие 1. Основы ООП/3. Домашние задания/task8_Glass/task.py
@@ -1,1 +1,43 @@
 # TODO Добавить методы add_water и remove_water
+from typing import Union
+class Glass:
+    def __init__(self, capacity_volume: Union[int, float], occupied_volume: Union[int, float]):
+        self.capacity_volume = None
+        self.init_capacity_volume(capacity_volume)
+        self.occupied_volume = None
+        self.init_occupied_volume(occupied_volume)
+
+    def init_capacity_volume(self, capacity_volume):
+        if not isinstance(capacity_volume, (int, float)):
+            raise TypeError
+        if not capacity_volume > 0:
+            raise ValueError
+        self.capacity_volume = capacity_volume
+
+    def init_occupied_volume(self, occupied_volume):
+        if not isinstance(occupied_volume, (int, float)):
+            raise TypeError
+        if not occupied_volume > 0:
+            raise ValueError
+        self.occupied_volume = occupied_volume
+
+    def add_water(self, add_water):
+        if add_water + self.occupied_volume > self.capacity_volume:
+            raise ValueError("Больше, чем возможно")
+        if not isinstance(add_water, (int, float)):
+            raise TypeError("Введите число")
+        self.occupied_volume = self.occupied_volume + add_water
+    def remove_water(self, remove_water):
+        if remove_water > self.occupied_volume:
+            raise ValueError("Больше, чем возможно")
+        if not isinstance(remove_water, (int, float)):
+            raise TypeError("Введите число")
+        self.occupied_volume = self.occupied_volume - remove_water
+
+if __name__ == "__main__":
+    glass = Glass(200, 100)  # экземпляр класса
+    print(glass.capacity_volume, glass.occupied_volume)
+    glass.remove_water(50)
+    print(glass.capacity_volume, glass.occupied_volume)
+    glass.add_water(100)
+    print(glass.capacity_volume, glass.occupied_volume)
--- Занятие 1. Основы ООП/3. Домашние задания/task9_Date/task.py
+++ Занятие 1. Основы ООП/3. Домашние задания/task9_Date/task.py
@@ -1,1 +1,16 @@
 # TODO class Date
+class Date:
+    def __init__(self, day, month, year):
+        self.day = day
+        self.month = month
+        self.year = year
+
+    def __repr__(self):
+        return f"Data({self.day}, {self.month}, {self.year})"
+
+    def __str__(self):
+        return f"{self.day:02}/{self.month:02}/{self.year}"
+
+if __name__ == "__main__":
+    data_1 = Date(3, 3, 2021)
+    print(data_1)
